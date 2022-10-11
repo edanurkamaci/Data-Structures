bbf67ce248a0465fc45e6a494a2c4d487336fb70
Eda Kamacı <152120191005@ogrenci.ogu.edu.tr>
2022-10-11 14:20:04 +0300

    LAB1 questions solved and added.

---
+++ DATA STRUCTURE LAB 1/DATA STRUCTURE LAB 1/Q4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+//COMPLEXITY = N-2
+
+string recursive(int&,int&, int&, int&);
+
+int main()
+{
+	int n;
+	int a = 0, b = 1,temp=0;
+
+	cout << "Enter the number of terms of series: ";
+	cin >> n;
+	n -= 2;
+
+	cout << "Fibonnaci Series: ";
+	cout<< recursive(a, b, temp, n);
+	cout << endl << endl;
+
+	return 0;
+}
+
+string recursive(int& a, int& b, int& temp, int& n)
+{
+	if (n == 0) return "";
+	if (a == 0 && b == 1) cout << a << "    " << b << "    ";
+
+	temp = a + b;
+	cout << temp<<"    ";
+
+	a = b; b = temp; n -= 1;
+
+	return  recursive(a, b, temp, n);
+}
